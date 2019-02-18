0407074123756fb1e9739188050a522d6fcd145e
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-02-18 23:10:13 +0530

    Editing Sequences
    Editing Sequences in Python

---
+++ Editing Sequences.py
@@ -0,0 +1,33 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> [1,2,3,4]+[5,6,7,8,9]
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> "Gayathri "+"Buddhika"
+'Gayathri Buddhika'
+>>> [4,6,7]+'Buddhika'
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    [4,6,7]+'Buddhika'
+TypeError: can only concatenate list (not "str") to list
+>>> 'uni'*10
+'uniuniuniuniuniuniuniuniuniuni'
+>>> 20*5
+100
+>>> [20]*5
+[20, 20, 20, 20, 20]
+>>> name='hjbvuirifbiu'
+>>> 'a' in name
+False
+>>> 'j' in name
+True
+>>> family=['dad','mom','sis']
+>>> 'bro' in family
+False
+>>> 'mom' in famiily
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    'mom' in famiily
+NameError: name 'famiily' is not defined
+>>> 'mom'in family
+True
+>>> 
